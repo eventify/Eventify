aa80bd419eaa242fae8887f3ef1b5ef274a77717
Dan Perez <perezda@gmail.com>
2011-07-17 11:46:01 -0700

    After create, story is now published too

--- Eventify/EventNewViewController.m
+++ Eventify/EventNewViewController.m
@@ -35,13 +35,29 @@
     [request startSynchronous];
     int statusCode = [request responseStatusCode];
     NSString *statusMessage = [request responseStatusMessage];
-    NSLog(@"status code = %d", statusCode);
-    NSLog(@"status message = %@", statusMessage);
+    NSLog(@"create status code = %d", statusCode);
+    NSLog(@"create status message = %@", statusMessage);
     
     [storyTitle resignFirstResponder];
     
     //if something was created, dismiss this VC and push the search controller.
     if (statusCode == 200) {
+        //if created then publish it and push 2 controllers
+        NSString *string = [NSString stringWithFormat:@"http://storify.com/eventify1/%@/publish", storyTitle.text];
+        NSURL *url = [NSURL URLWithString:string];
+        ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:url];
+        [request setPostValue:@"eventify1" forKey:@"authorUsername"];
+        [request setPostValue:storyTitle.text forKey:@"title"];
+        [request setPostValue:@"eventify1" forKey:@"username"];
+        [request setPostValue:@"d6e2ba3299dc8e372f2f9788e1c8cb95" forKey:@"api_key"];
+        [request setRequestMethod:@"POST"];
+        [request startSynchronous];
+        int statusCode = [request responseStatusCode];
+        NSString *statusMessage = [request responseStatusMessage];
+        NSLog(@"publish status code = %d", statusCode);
+        NSLog(@"publish status message = %@", statusMessage);
+        
+        
         EventViewController *evc = [[EventViewController alloc] init];
         SearchViewController *svc = [[SearchViewController alloc] init];
         
@@ -63,6 +79,8 @@
         
     }
 }
+
+
 
 - (IBAction) getStoriesButtonPressed:(id)sender 
 {
